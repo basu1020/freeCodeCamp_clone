2037789aec34653e0a3103d268f23758f334d545
basu1020 <bvp1020mu@gmail.com>
2022-06-27 00:34:15 +0530

    "27-06-2022"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,73 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <>
-    <footer>
-        
-    </footer>
-    </>
-  )
+    return (
+        <>
+            <footer>
+                <div className="container">
+                    <div className="footer__about">
+                        <p>freeCodeCamp is a donor-supported tax-exempt 501(c)(3) nonprofit organization (United States Federal Tax Identification Number: 82-0779546)</p>
+
+                        <p>Our mission: to help people learn to code for free. We accomplish this by creating thousands of videos, articles, and interactive coding lessons - all freely available to the public. We also have thousands of freeCodeCamp study groups around the world.</p>
+
+                        <p>Donations to freeCodeCamp go toward our education initiatives, and help pay for servers, services, and staff.</p>
+
+                        <p>You can</p><a href=""> make a tax-deductible donation here.</a>
+                    </div>
+                    <div className="trending-guides">
+                        <div className="trending-guides__title">
+                            <p>Trending Guides</p>
+                        </div>
+                        <div className="footer__trending-guides--links">
+
+                            <div className="first-section">
+                                <ul>
+                                    <li><a href=''>Zoom Screen Sharing</a></li>
+                                    <li><a href=''>Decimal Place Value</a></li>
+                                    <li><a href=''>How to Get Into BIOS</a></li>
+                                    <li><a href=''>String to Int in C++</a></li>
+                                    <li><a href=''>What is msmpeng.exe</a></li>
+                                    <li><a href=''>Facetime Not Working</a></li>
+                                    <li><a href=''>Desktop Icons Missing</a></li>
+                                    <li><a href=''>How to Copy and Paste</a></li>
+                                    <li><a href=''>Delete a Page in Word</a></li>
+                                    <li><a href=''>vcruntime140.dll Error</a></li>
+                                </ul>
+                            </div>
+                            <div className="second-section">
+                                <ul>
+                                    <li><a href="">C++ Vector</a></li>
+                                    <li><a href="">What is CPU</a></li>
+                                    <li><a href="">IPV4 vs IPV6</a></li>
+                                    <li><a href="">What is IPTV</a></li>
+                                    <li><a href="">HTML Font Size</a></li>
+                                    <li><a href="">Change Mouse DPI</a></li>
+                                    <li><a href="">How to Make a GIF</a></li>
+                                    <li><a href=""> Git Rename Branch</a></li>
+                                    <li><a href="">Make a Video Game</a></li>
+                                    <li><a href="">CSS Media Queries</a></li>
+                                </ul>
+                            </div>
+                            <div className="third-section">
+                                <ul>
+                                    <li><a href="">How to Open .dat Files</a></li>
+                                    <li><a href="">Record Calls on iPhone</a></li>
+                                    <li><a href="">Ascending vs Descending</a></li>
+                                    <li><a href="">HTML Email Link Tutorial</a></li>
+                                    <li><a href="">Python List Comprehension</a></li>
+                                    <li><a href="">Password Protect Zip File</a></li>
+                                    <li><a href="">Restore Deleted Word File</a></li>
+                                    <li><a href="">Software Engineering Guide</a></li>
+                                    <li><a href="">How to Find Your IP Address</a></li>
+                                    <li><a href="">How to Find iPhone Download</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
 }
 
 export default Footer
