66085074c3ca7f246ade6cd48f1df1136cae3dfb
basu1020 <bvp1020mu@gmail.com>
2022-07-24 23:13:13 +0530

    refactord some code

--- src/components/BackEnd.js
+++ src/components/BackEnd.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import Codemirror from '@uiw/react-codemirror'
+import { javascript } from '@codemirror/lang-javascript'
 
 const BackEnd = () => {
   return (
-    <div className='body'>BackEnd</div>
+    <>
+      <div className="container-dark">
+        <div className='darker'>
+          <div className="darker-instructions">
+            <div className="darker-instructions__text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, illo. Mollitia, molestiae quod odit facere odio pariatur officiis facilis accusantium! Quaerat commodi tempora nisi, quis quam recusandae minus soluta beatae magni amet totam. Numquam.</p>
+            </div>
+          </div>
+        </div>
+        <div className='darker'>
+          <Codemirror
+            theme={"dark"}
+            height={"60vh"}
+            width={"42vw"}
+            extensions={[javascript({ jsx: true })]}
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Responsive.js
+++ src/components/Responsive.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Codemirror from "@uiw/react-codemirror"
+import { html } from "@codemirror/lang-html"
 
 const Responsive = () => {
   return (
-    <div className="body">Responsive</div>
+    <div className="darker">
+      <Codemirror
+        height='60vh'
+        theme={"dark"}
+        extensions={[html()]}
+      />
+    </div>
   )
 }
 
